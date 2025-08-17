ae752e151d9c894166c4fb1abf9fe7060a92559d
Matias <matiasezequielsmania@gmail.com>
2025-08-16 22:56:44 -0300

    navbar

--- Front/src/app/components/navbar/navbar.component.ts
+++ Front/src/app/components/navbar/navbar.component.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+  menuOpen = false;
+
+
+  constructor() {
+  }
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+
+  closeMenu() {
+    setTimeout(() => {
+      this.menuOpen = false;
+    }, 200);
+  }
 
 }
