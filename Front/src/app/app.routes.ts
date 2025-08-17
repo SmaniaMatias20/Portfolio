f14e8527cf481ba29b42642e59052c1ed7b009b3
Matias <matiasezequielsmania@gmail.com>
2025-08-16 23:12:04 -0300

    pages

--- Front/src/app/app.routes.ts
+++ Front/src/app/app.routes.ts
@@ -6,6 +6,26 @@ export const routes: Routes = [
         loadComponent: () =>
             import('./pages/home/home.component').then((m) => m.HomeComponent),
     },
+    {
+        path: 'profile',
+        loadComponent: () =>
+            import('./pages/profile/profile.component').then((m) => m.ProfileComponent),
+    },
+    {
+        path: 'projects',
+        loadComponent: () =>
+            import('./pages/projects/projects.component').then((m) => m.ProjectsComponent),
+    },
+    {
+        path: 'experience',
+        loadComponent: () =>
+            import('./pages/experience/experience.component').then((m) => m.ExperienceComponent),
+    },
+    {
+        path: 'contact',
+        loadComponent: () =>
+            import('./pages/contact/contact.component').then((m) => m.ContactComponent),
+    },
     { path: '**', redirectTo: 'home' }
 ];
 
