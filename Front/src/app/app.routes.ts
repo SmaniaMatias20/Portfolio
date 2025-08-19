4c51a0f7660e367941787af06600a298e334796f
Matias <matiasezequielsmania@gmail.com>
2025-08-18 21:00:44 -0300

    navbar border

--- Front/src/app/app.routes.ts
+++ Front/src/app/app.routes.ts
@@ -21,11 +21,6 @@ export const routes: Routes = [
         loadComponent: () =>
             import('./pages/experience/experience.component').then((m) => m.ExperienceComponent),
     },
-    {
-        path: 'contact',
-        loadComponent: () =>
-            import('./pages/contact/contact.component').then((m) => m.ContactComponent),
-    },
     { path: '**', redirectTo: 'home' }
 ];
 
